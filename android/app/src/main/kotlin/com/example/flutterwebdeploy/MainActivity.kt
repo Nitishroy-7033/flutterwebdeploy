cec657f31354864668c740635c204f8e4f4aa11c
Nitishroy-7033 <nitishr833@gmail.com>
2024-10-02 21:54:36 +0530

    code added

---
+++ android/app/src/main/kotlin/com/example/flutterwebdeploy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterwebdeploy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepOrange.shade100,
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          centerTitle: true,
+          title: const Text(
+            "FLUTTER HERO",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: GridView.builder(
+              itemCount: 20,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                mainAxisSpacing: 30,
+                crossAxisSpacing: 30,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                    decoration:
+                        BoxDecoration(color: Colors.deepOrange.shade200));
+              }),
+        ));
+  }
+}
